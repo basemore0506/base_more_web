ec9c8a5eeb37631b7ebd30782831c3f16ea147f1
poisson0106 <poisson0106@gmail.com>
2021-05-09 22:10:24 +0800

    feat: First page frame

---
+++ src/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="video-wrapper">
+    <div class="tab-nav">
+      <div class="tab-logo">
+        <div class="logo">Logo</div>
+      </div>
+      <div class="tab-content">111</div>
+      <div class="tab-content">222</div>
+      <div class="tab-content">333</div>
+    </div>
+    <div class="video">
+      <video src="../video/movie.mp4" muted autoplay loop/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.video-wrapper {
+  width: 100vw;
+  height: 100vh;
+}
+
+.tab-nav {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-content: space-between;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+}
+
+.tab-content {
+  flex: 1;
+  background-color:white;
+  opacity: 0.5;
+  height: 80px;
+}
+
+.tab-logo {
+  width: 100vw;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+.logo {
+  margin-top: 2%;
+  background-color: wheat;
+  height: 100%;
+  width: 100px;
+  opacity: 0.5;
+  text-align: center;
+  line-height: 50px;
+}
+
+.video{
+  width: 100vw;
+  height: 100vh;
+}
+
+video{  
+    width: 100vw;
+    height: 100vh;
+    object-fit: fill;
+}  
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="home_page">
+    <Banner />
+    <el-main class="remove_padding">
+      <el-row>
+        <el-col :span="24">bbb</el-col>
+      </el-row>
+    </el-main>
+  </div>
+</template>
+
+<script>
+import Banner from "./Banner.vue"
+
+export default {
+name: 'home_page',
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style>
+.remove_padding{
+  padding:0 0 0 0 !important
+}
+</style>
